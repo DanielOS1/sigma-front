277a34abb2a5f56abcb5aaa98c1b68a1754d4274
DanielOS <danielolivares60@gmail.com>
2025-01-02 17:43:12 -0300

    environment setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'sigma-front' title`, () => {
+  it(`should have the 'sigma-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sigma-front');
+    expect(app.title).toEqual('sigma-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sigma-front');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sigma-frontend');
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,4 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
-  // Hola
   .catch((err) => console.error(err));
