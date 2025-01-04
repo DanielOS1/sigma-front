4e34f886c4dad88426cffeff7e3ccc5c4de9776b
DanielOS <danielolivares60@gmail.com>
2025-01-03 21:25:13 -0300

    feat: basic sesing for login screen

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-authentication',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatInputModule, MatButtonModule,CommonModule, FormsModule],
   templateUrl: './authentication.component.html',
   styleUrl: './authentication.component.scss'
 })
 export class AuthenticationComponent {
+    rut: string  = '';
+    password: string = '';
+    showPassword: boolean = false;
+
+    onRutChange(): void {
+      const rutFocus = '11.111.111-1';
+      this.showPassword = this.rut === rutFocus;
+    }
 
 }
