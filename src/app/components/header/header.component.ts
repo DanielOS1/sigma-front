2e21f098813606ea4c5667c958ce501cc6fa4b1e
DanielOS <danielolivares60@gmail.com>
2025-01-07 21:19:43 -0300

    new alert log out

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-header',
@@ -12,11 +13,12 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private toastr: ToastrService) {}
 
   
   logout(): void {
     this.authService.logout();
+    this.toastr.success('Sesión cerrada con éxito', 'Éxito');
     this.router.navigate(['/auth']);
   }
 
